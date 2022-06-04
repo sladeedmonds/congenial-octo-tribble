f0756b593949708f57c73785648a4913a9f01f01
Slade MSFT <sladeedmonds@microsoft.com>
2022-06-04 18:08:07 -0400

    kv needs tenant id

--- main/keyvault.tf
+++ main/keyvault.tf
@@ -11,5 +11,6 @@ resource "azurerm_key_vault" "kv-sandbox" {
   name                = "${random_pet.kv.id}"
   location            = azurerm_resource_group.rg-keyvault.location
   resource_group_name = azurerm_resource_group.rg-keyvault.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name = "standard"
   }
