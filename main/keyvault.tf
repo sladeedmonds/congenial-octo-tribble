ac809b92c3b6fc0bfe89fd7d8174a21bfc134bca
Slade MSFT <sladeedmonds@microsoft.com>
2022-06-04 18:04:34 -0400

    tf kv resource name oops

--- main/keyvault.tf
+++ main/keyvault.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg-keyvault" {
   location  = var.resource_group_location
   }
 
-resource "azurerm_key_vault" "bastion-default" {
+resource "azurerm_key_vault" "kv-sandbox" {
   name                = "${random_pet.kv.id}"
   location            = azurerm_resource_group.rg-keyvault.location
   resource_group_name = azurerm_resource_group.rg-keyvault.name
