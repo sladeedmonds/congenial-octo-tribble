13c3e3e4ae6e16c761a53be778af497f618db9eb
Slade Edmonds <slade@msft>
2022-06-27 10:38:50 -0400

    vars

--- main/storage.tf
+++ main/storage.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "rg-tf-state" {
 resource "azurerm_storage_account" "tf-storage" {
   name                     = random_string.tf-storage-name.id
   resource_group_name      = azurerm_resource_group.rg-tf-state.name
-  location                 = var.resource_group_location
+  location                 = azurerm_resource_group.rg-tf-state.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
